90d0c5119ce9bfed3208ac766ffe8171108801de
ScarlettBlaize <cutiepie.ats@gmail.com>
2023-02-26 13:18:28 -0500

    Update CreateProfile.js
    updated user login page

--- spotify-friending/src/CreateProfile.js
+++ spotify-friending/src/CreateProfile.js
@@ -4,19 +4,37 @@ export default function CreateProfile() {
     return (
         <>
             <h1>Create Profile</h1>
-            <p>Write your 5 favorite songs:</p>
-            <label htmlFor="song1">Song 1:</label>
-            <input id="song1" />
-            <label htmlFor="song2">Song 2:</label>
-            <input id="song2" />
-            <label htmlFor="song3">Song 3:</label>
-            <input id="song3" />
-            <label htmlFor="song4">Song 4:</label>
-            <input id="song4" />
-            <label htmlFor="song5">Song 5:</label>
-            <input id="song5" />
-            <label htmlFor="userId">User ID:</label>
+            <p>Please enter your username</p>
+
+            <label htmlFor="userId">User ID: </label>
             <input id="userId" />
+
+            <p>Please enter your password</p>
+
+            <label htmlFor="password">Password: </label>
+            <input id="password" />
+
+            <p>Write your 5 favorite songs:</p>
+            <label htmlFor="song1">Song 1: </label>
+            <input id="song1" />
+            <br></br>
+            <br></br>
+            <label htmlFor="song2">Song 2: </label>
+            <input id="song2" />
+            <br></br>
+            <br></br>
+            <label htmlFor="song3">Song 3: </label>
+            <input id="song3" />
+            <br></br>
+            <br></br>
+            <label htmlFor="song4">Song 4: </label>
+            <input id="song4" />
+            <br></br>
+            <br></br>
+            <label htmlFor="song5">Song 5: </label>
+            <input id="song5" />
+            <br></br>
+            <br></br>
         </>
     );
 }
