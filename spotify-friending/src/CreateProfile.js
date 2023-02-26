bf65391065769166fafc84a750db38db53071323
ScarlettBlaize <cutiepie.ats@gmail.com>
2023-02-26 13:43:37 -0500

    updated CreateProfile
    updated user login

--- spotify-friending/src/CreateProfile.js
+++ spotify-friending/src/CreateProfile.js
@@ -1,63 +1,76 @@
 // __ -> Component / View
+import './index.css';
 
 export default function CreateProfile() {
     return (
         <>
-            <h1>Create Profile</h1>
-            <p>Please enter your username</p>
+            <div className="flex-container width-100 column">
+                <h1>Create Profile</h1>
 
-            <label htmlFor="userId">User ID: </label>
-            <input id="userId" />
+                <h4>Please enter your username</h4>
 
-            <p>Please enter your password</p>
+                <label htmlFor="userId">User ID: </label>
+                <input id="userId" />
 
-            <label htmlFor="password">Password: </label>
-            <input id="password" />
+                <h4>Please enter your password</h4>
 
-            <p>Write your 5 favorite songs:</p>
-            <label htmlFor="song1">Song 1: </label>
-            <input id="song1" />
-            <br></br>
-            <br></br>
-            <label htmlFor="song2">Song 2: </label>
-            <input id="song2" />
-            <br></br>
-            <br></br>
-            <label htmlFor="song3">Song 3: </label>
-            <input id="song3" />
-            <br></br>
-            <br></br>
-            <label htmlFor="song4">Song 4: </label>
-            <input id="song4" />
-            <br></br>
-            <br></br>
-            <label htmlFor="song5">Song 5: </label>
-            <input id="song5" />
+                <label htmlFor="password">Password: </label>
+                <input id="password" />
+            </div>
 
-            <p>Write your 5 favorite artists:</p>
-            
-            <label htmlFor="artist1">Artist 1: </label>
-            <input id="artist1" />
-            <br></br>
-            <br></br>
+            <div className="flex-container row">
+                <div className="mx-20">
+                    <h5>Write your 5 favorite songs:</h5>
+                    <label htmlFor="song1">Song 1: </label>
+                    <input id="song1" />
+                    <br></br>
+                    <br></br>
+                    <label htmlFor="song2">Song 2: </label>
+                    <input id="song2" />
+                    <br></br>
+                    <br></br>
+                    <label htmlFor="song3">Song 3: </label>
+                    <input id="song3" />
+                    <br></br>
+                    <br></br>
+                    <label htmlFor="song4">Song 4: </label>
+                    <input id="song4" />
+                    <br></br>
+                    <br></br>
+                    <label htmlFor="song5">Song 5: </label>
+                    <input id="song5" />
+                </div>
 
-            <label htmlFor="artist2">Artist 2: </label>
-            <input id="artist2" />
-            <br></br>
-            <br></br>
+                <div className="mx-20">
+                    <h5>Write your 5 favorite artists:</h5>
 
-            <label htmlFor="artist3">Artist 3: </label>
-            <input id="artist3" />
-            <br></br>
-            <br></br>
+                    <label htmlFor="artist1">Artist 1: </label>
+                    <input id="artist1" />
+                    <br></br>
+                    <br></br>
 
-            <label htmlFor="artist4">Artist 4: </label>
-            <input id="artist4" />
-            <br></br>
-            <br></br>
+                    <label htmlFor="artist2">Artist 2: </label>
+                    <input id="artist2" />
+                    <br></br>
+                    <br></br>
 
-            <label htmlFor="artist5">Artist 5: </label>
-            <input id="artist5" />
+                    <label htmlFor="artist3">Artist 3: </label>
+                    <input id="artist3" />
+                    <br></br>
+                    <br></br>
+
+                    <label htmlFor="artist4">Artist 4: </label>
+                    <input id="artist4" />
+                    <br></br>
+                    <br></br>
+
+                    <label htmlFor="artist5">Artist 5: </label>
+                    <input id="artist5" />
+                </div>
+            </div>
+
+
+
         </>
     );
 }
